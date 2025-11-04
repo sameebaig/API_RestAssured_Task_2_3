f9675185e5785bb4e3411e867e3ac9f30bd3933e
sameebaig <samee.baig@venturedive.com>
2025-11-04 14:03:02 +0500

    Add ConfigReader class for reading properties

---
+++ src/test/java/api.utilities/ConfigReader.java
@@ -0,0 +1,22 @@
+package api.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties prop;
+    static {
+        try {
+            FileInputStream fis = new FileInputStream("src/test/resources/routes.properties");
+            prop = new Properties();
+            prop.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static String getProperty(String key) {
+        return prop.getProperty(key);
+    }
+}
